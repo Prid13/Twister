60382d97f2b23ed919a7b3264af3f835bfc77d3b
Prid13 <prid_outing@hotmail.com>
2018-04-02 07:46:28 +0200

    Finished Handler class

--- handler_class.php
+++ handler_class.php
@@ -1,17 +1,43 @@
 <?php
 
+include("vendor/simple_html_dom.php");
+
 class Handler {
 
 	public $url;
 	public $initFunc;
 	public $handlerFunc;
+	private $min_name;
+	private $items;
+	private $items_saved;
+	private $firstRead;
+	public $diffTxt;
 
 	public function __construct($url){
 		$this->url = $url;
+		$this->min_name = urlencode(basename($url));
 	}
 	
-	public function start(){
-		call_user_func($this->initFunc, "HELLO WORLD");
+	public function start(){		
+		$html = file_get_html($this->url);
+		
+		$this->items = array();
+		$this->firstRead = !$this->readFileContents();
+		
+		if($this->firstRead){
+			if(isset($this->initFunc)){
+				$initFunc = $this->initfunc;
+				$initFunc($html, $this->items);
+			} else {
+				$handlerFunc = $this->handlerFunc;
+				$handlerFunc($html, $this->items);
+			}
+		} else {
+			$handlerFunc = $this->handlerFunc;
+			$handlerFunc($html, $this->items);
+		}
+		
+		$this->saveDifferences();
 	}
 	
 	public function setInitFunction($anonFunc){
@@ -20,6 +46,39 @@ class Handler {
 	
 	public function setHandler($anonFunc){
 		$this->handlerFunc = $anonFunc;
+	}
+	
+	private function readFileContents(){
+		$this->items_saved = [];
+
+		$file_exists = file_exists('db/' . $this->min_name . '.txt');
+		
+		if($file_exists){
+			$fp = fopen('db/' . $this->min_name . '.txt', 'r');
+			$content = fread($fp, filesize('db/' . $this->min_name . '.txt'));
+			fclose($fp);
+
+			$this->items_saved = explode("\r\n", $content);
+		} else {
+			return false;
+		}
+	}
+	
+	private function saveDifferences(){
+		if($this->firstRead || count(array_diff($this->items_saved, $this->items)) > 0){
+			$text = "";
+			
+			for($i = 0; $i < count($this->items)-1; $i++)
+				$text .= $this->items[$i] . "\r\n";
+			
+			$text .= $this->items[count($this->items)-1];
+			
+			$this->diffTxt = $text;
+			
+			$fp = fopen('db/' . $this->min_name . '.txt', 'w');
+			fwrite($fp, $text);
+			fclose($fp);
+		}
 	}
 	
 }
--- twists/sawano_ost.php
+++ twists/sawano_ost.php
@@ -4,54 +4,20 @@ $url = "https://en.wikipedia.org/wiki/Hiroyuki_Sawano";
 
 $twist = new Handler($url);
 
-$twist->setInitFunction(function($s){
-	echo $s;
-});
-
-$twist->start();
-
-/*$twist->setInitFunction(function(){
-	
-});
-
-$twist->setHandler(function(){
+$twist->setHandler(function($html, &$items){
 	foreach ($html->find('.wikitable') as $tables) {
 		$tableData = $tables->find("tbody tr");
 		
 		if(strpos(strtolower($tableData[0]), 'anime') !== false){
 			for($i = 1; $i < count($tableData); $i++){
-				$works[] = $tableData[$i]->first_child()->plaintext;
+				$items[] = $tableData[$i]->first_child()->plaintext;
 			}
 		}
 	}
 });
 
-$min_name = urlencode(basename($url));
-$html = file_get_html($url);
+$twist->start();
 
-$works_saved = [];
-
-$fp = fopen('db/' . $min_name . '.txt', 'r');
-$content = fread($fp, filesize('twists/' . $min_name . '.txt'));
-fclose($fp);
-
-$works_saved = explode("\r\n", $content);
-
-$works = [];
-
-
-// this should be automated
-if(count(array_diff($works_saved, $works)) > 0){
-	$text = "";
-	
-	for($i = 0; $i < count($works)-1; $i++)
-		$text .= $works[$i] . "\r\n";
-	
-	$text .= $works[count($works)-1];
-	
-	$fp = fopen('db/' . $min_name . '.txt', 'w');
-	fwrite($fp, $text);
-	fclose($fp);
-}*/
+var_dump($twist->diffTxt);
 
 ?>
